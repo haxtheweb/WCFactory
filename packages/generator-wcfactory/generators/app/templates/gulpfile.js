bdf825cd1088b5aeb568174efd24fafa6a08286f
btopro <btopro@outlook.com>
2018-09-25 20:34:41 -0400

    #12 analyzer working

--- packages/generator-wcfactory/generators/app/templates/gulpfile.js
+++ packages/generator-wcfactory/generators/app/templates/gulpfile.js
@@ -55,7 +55,6 @@ gulp.task("merge", () => {
           oneLineFile
         );
         props = fs.readFileSync(path.join("./src", propertiesUrl));
-        props = stripCssComments(props).trim();
 <%_ } _%>
         // pull together styles from url
         const [
@@ -104,15 +103,13 @@ gulp.task("build", () => {
 });
 // run polymer analyze to generate documentation
 gulp.task("analyze", () => {
-  const spawn = require("child_process").spawn;
-  const out = fs.openSync('./analysis.json', 'a');
-  const err = fs.openSync('./analysis-error.json', 'a');
-  let child = spawn("polymer", ["analyze --i " + packageJson.wcfactory.elementName], {
-    stdio: ['ignore', out, err]
-  });
-  return child.on("close", function (code) {
-    console.log("child process exited with code " + code);
-  });
+  var exec = require('child_process').exec;
+  return exec('polymer analyze --input demo/index.html > analysis.json',
+    function (error, stdout, stderr) {
+      if (error !== null) {
+        console.log('exec error: ' + error);
+      }
+    });
 });
 // copy from the built locations pulling them together
 gulp.task("compile", () => {
@@ -150,7 +147,7 @@ gulp.task("compile", () => {
 gulp.task("watch", () => {
   return gulp.watch(
     "./src/*",
-    gulp.series("merge")
+    gulp.series("merge", "analyze")
   );
 });
 
