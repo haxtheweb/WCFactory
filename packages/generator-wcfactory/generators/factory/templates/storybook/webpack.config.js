04c2937a12d0fefe37c3c1b80c683150e89769f3
btopro <btopro@outlook.com>
2018-11-30 15:56:14 -0500

    resolve import address

--- packages/generator-wcfactory/generators/factory/templates/storybook/webpack.config.js
+++ packages/generator-wcfactory/generators/factory/templates/storybook/webpack.config.js
@@ -1,24 +1,10 @@
 const path = require("path");
-
 module.exports = (baseConfig, env, defaultConfig) => {
-  defaultConfig.resolve.alias = {
-    "../../whatwg-fetch/fetch.js": path.join(
-      __dirname,
-      "../node_modules/whatwg-fetch/fetch.js"
-    ),
-    "../../numeral/min/numeral.min.js": path.join(
-      __dirname,
-      "../node_modules/numeral/min/numeral.min.js"
-    ),
-    "../../numeral/numeral.js": path.join(
-      __dirname,
-      "../node_modules/numeral/numeral.js"
-    ),
-    "../../dialog-polyfill/dialog-polyfill.js": path.join(
-      __dirname,
-      "../node_modules/dialog-polyfill/dialog-polyfill.js"
+  defaultConfig.module.rules.push({
+    test: [/\.js$/],
+    loader: require.resolve(
+      "@open-wc/webpack/loaders/import-meta-url-loader.js"
     )
-  };
-
+  });
   return defaultConfig;
 };
