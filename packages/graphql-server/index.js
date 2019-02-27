3e7b00126e9830bd002369b55eb69ff32d305ec2
heyMP <mgp140@gmail.com>
2019-02-27 09:20:31 -0500

    chore: comment cleanup

--- packages/graphql-server/index.js
+++ packages/graphql-server/index.js
@@ -4,20 +4,16 @@ const pubsub = new PubSub()
 
 const STREAM_UPDATED = 'STREAM_UPDATED';
 
-// get factories
+/**
+ * SDK
+ */
 const getFactories = () => factoryOptions().map(i => Object.assign({ name: i.name, location: i.value}))
 const getFactoryElements = (location) => getElements(location).map(i => Object.assign(i, { factory: getFactories().find(i => i.location === location)}))
 
-// This is a (sample) collection of books we'll be able to query
-// the GraphQL server for.  A more complete example might fetch
-// from an existing data source like a REST API or database.
-
-// Type definitions define the "shape" of your data and specify
-// which ways the data can be fetched from the GraphQL server.
+/**
+ * Define Schema
+ */
 const typeDefs = gql`
-  # Comments in GraphQL are defined with the hash (#) symbol.
-
-  # This "Book" type can be used in other type declarations.
   type Factory {
     location: String!
     name: String
@@ -35,8 +31,6 @@ const typeDefs = gql`
     streamUpdated: String
   }
 
-  # The "Query" type is the root of all GraphQL queries.
-  # (A "Mutation" type will be covered later on.)
   type Query {
     factories: [Factory]
     elements(factoryLocation: String): [Element]!
@@ -47,8 +41,9 @@ const typeDefs = gql`
   }
 `;
 
-// Resolvers define the technique for fetching the types in the
-// schema.  We'll retrieve books from the "books" array above.
+/**
+ * Resolvers
+ */
 const resolvers = {
   Query: {
     factories: () => getFactories(),
@@ -68,13 +63,10 @@ const resolvers = {
   },
 };
 
-// In the most basic sense, the ApolloServer can be started
-// by passing type definitions (typeDefs) and the resolvers
-// responsible for fetching the data for those types.
+/**
+ * Create and start server
+ */
 const server = new ApolloServer({ cors: true, typeDefs, resolvers });
-
-// This `listen` method launches a web-server.  Existing apps
-// can utilize middleware options, which we'll discuss later.
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
