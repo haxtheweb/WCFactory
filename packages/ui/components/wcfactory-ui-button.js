8aa3c101df1543262beef46edb69c0b2a2d8da5b
heyMP <mgp140@gmail.com>
2019-04-01 14:13:51 -0400

    added create form

---
+++ packages/ui/components/wcfactory-ui-button.js
@@ -0,0 +1,99 @@
+import { LitElement, html } from 'lit-element';
+import '@polymer/paper-button/paper-button.js';
+
+class WCFactoryUIButton extends LitElement {
+  static get properties() {
+    return {
+      disabled: { type: Boolean, reflect: true }
+    }
+  }
+
+  constructor() {
+    super()
+    this.disabled = false
+  }
+
+  render() {
+    return html`
+      <style>
+      :host {
+        --wcfactory-ui-button-bg-color: #2C3E50;
+        --wcfactory-ui-button-color: #15B5E2;
+        display: block;
+      }
+      :host([disabled=""]) {
+        opacity: 0.2;
+      }
+      .btn-wrapper {
+        height: 110px;
+        width: calc(100% - 2.1em);
+        position: relative;
+        display: block;
+        padding: 0;
+        margin: 0;
+        margin-top: 20px;
+        margin-left: 1.05em;
+      }
+      :host(:not([disabled=""])) .btn-wrapper:focus .btn-inner,
+      :host(:not([disabled=""])) .btn-wrapper:hover .btn-inner {
+        top: -4px;
+        transform: scale(1, 1);
+        cursor: pointer;
+      }
+      .btn-wrapper__container {
+        border-bottom: 2px solid var(--wcfactory-ui-button-color);
+        position: absolute;
+        width: 100%;
+        height: 80px;
+      }
+      .btn-wrapper__container:before,
+      .btn-wrapper__container:after {
+        border-bottom: 2px solid var(--wcfactory-ui-button-color);
+        width: 96%;
+        left: 2%;
+        bottom: -8px;
+        content: "";
+        position: absolute;
+      }
+      .btn-wrapper__container:after {
+        width: 92%;
+        left: 4%;
+        bottom: -14px;
+      }
+      .btn-wrapper__container .btn-inner {
+        width: 104.2%;
+        height: 100%;
+        position: absolute;
+        top: 20px;
+        left: -2.1%;
+        border: 2px solid var(--wcfactory-ui-button-color);
+        box-sizing: border-box;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        letter-spacing: 1px;
+        text-transform: uppercase;
+        font-size: 18px;
+        background: var(--wcfactory-ui-button-bg-color);
+        transform: scale(0.96, 0.96);
+        transition: all .3s;
+        z-index: 4;
+      }
+      .btn-wrapper__container .btn-inner__text {
+        text-decoration: none;
+        color: var(--wcfactory-ui-button-color);
+      }
+      </style>
+      <paper-button class="btn-wrapper" .disabled=${this.disabled}>
+        <div class="btn-wrapper__container">
+          <div class="btn-inner">
+            <div class="btn-inner__text"><slot></slot></div>
+          </div>
+        </div>
+      </paper-button>
+    `;
+  }
+
+}
+
+customElements.define('wcfactory-ui-button', WCFactoryUIButton);
--- packages/ui/components/wcfactory-ui-factory-create.js
+++ packages/ui/components/wcfactory-ui-factory-create.js
@@ -1,16 +1,111 @@
 import { LitElement, html } from 'lit-element';
+import '@polymer/paper-input/paper-input.js';
+import '@polymer/iron-form/iron-form.js';
+import './wcfactory-ui-button.js'
+import validate from 'is-valid-element-name';
+import 'lit-apollo/elements/apollo-mutation-element.js';
 
 class WCFactoryUIFactoryCreate extends LitElement {
+  static get properties() {
+    return {
+      form: { type: Object },
+      name: { type: String },
+      description: { type: String },
+      gitOrg: { type: String },
+      npmOrg: { type: String },
+      isValid: { type: Boolean },
+      loading: { type: Boolean }
+    }
+  }
 
   constructor() {
     super()
+    this.isValid = false
+    this.name = ''
+    this.description = ''
+    this.gitOrg = ''
+    this.npmOrg = ''
   }
 
-  render() {
-    // notice location changed
-    return html`hi there form`
+  updated(changed) {
+    if (
+      this.name !== '' &&
+      this.description !== '' &&
+      this.gitOrg !== '' &&
+      this.npmOrg !== ''
+    ) {
+      this._validateForm()
+    }
+  }
+
+  render () {
+    return html`
+      <style>
+        :host {
+          display: block;
+        }
+        #footer {
+          margin-top: 5vw;
+        }
+        #repo-info {
+          display: flex;
+          justify-content: space-between;
+        }
+        #wrapper {
+          background-color: white;
+          padding: 1vw;
+          color: #333;
+        }
+        #info {
+          margin-top: 1vw;
+        }
+      </style>
+      <div id="wrapper">
+        <iron-form id="form">
+          <form>
+            <paper-input always-float-label required label="name" auto-validate pattern="^[a-z](?:[\-\.0-9_a-z\xB7\xC0-\xD6\xD8-\xF6\xF8-\u037D\u037F-\u1FFF\u200C\u200D\u203F\u2040\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]|[\uD800-\uDB7F][\uDC00-\uDFFF])*-(?:[\-\.0-9_a-z\xB7\xC0-\xD6\xD8-\xF6\xF8-\u037D\u037F-\u1FFF\u200C\u200D\u203F\u2040\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]|[\uD800-\uDB7F][\uDC00-\uDFFF])*$" @value-changed=${(e) => this.name = e.detail.value}></paper-input>
+            <paper-input always-float-label required label="description" @value-changed=${(e) => this.description = e.detail.value}></paper-input>
+            <paper-input always-float-label required label="gitOrg" auto-validate pattern="[a-z\-]*" @value-changed=${(e) => this.gitOrg = e.detail.value}></paper-input>
+            <paper-input always-float-label required label="npmOrg" auto-validate pattern="[a-z\-]*" @value-changed=${(e) => this.npmOrg = e.detail.value}></paper-input>
+          <form>
+        </iron-form>
+
+        <div id="info">
+          <div id="name"><h2>🏭 ${this.name}</h2></div>
+          <div id="repo-info">
+            <div id="git-repo">
+              Git Repo: <br>
+              git:github.com/${this.gitOrg ? this.gitOrg : '<git org>'}/${this.name ? this.name : '<name>'}
+            </div>
+            <div id="npm-repo" ?data-filled=${this.npmOrg}>
+              NPM Repo: <br>
+              @${this.npmOrg ? `${this.npmOrg}` : ''}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="create" >
+        <wcfactory-ui-button @click=${() => this._validateForm()} .disabled=${!this.isValid}>Create Factory</wcfactory-ui-button>
+      </div>
+
+    `;
+  }
+  
+  _validateForm() {
+    const form = this.shadowRoot.querySelector('#form')
+    const valid = form.validate()
+    this.isValid = valid
   }
 
 }
 
 customElements.define('wcfactory-ui-factory-create', WCFactoryUIFactoryCreate);
+      // <script type="application/graphql">
+      //   mutation ($id: ID!)
+      //     user(id: $id) {
+      //       name
+      //       picture
+      //     }
+      //   }
+      // </script>
