a2823492ab1164d1698258f35ff1a2831235b931
btopro <btopro@outlook.com>
2019-09-16 14:46:48 -0400

    hax no longer requires this to work!

--- packages/generator-wcfactory/generators/element/index.js
+++ packages/generator-wcfactory/generators/element/index.js
@@ -79,12 +79,6 @@ module.exports = class extends Generator {
     this.props.propsListString = JSON.stringify(this.props.propsList, null, '  ')
     // work on HAX integration if requested
     if (this.props.useHAX) {
-      // include statement for top of files
-      this.props.includesString += 'import { HAXWiring } from "@lrnwebcomponents/hax-body-behaviors/lib/HAXWiring.js"';
-      // package dependency
-      this.props.libraryDependencies += `"@lrnwebcomponents/hax-body-behaviors":"latest",`;
-      // load props in from this dynamically generated function call
-      this.props.connectedString = 'this.HAXWiring = new HAXWiring();' + "\n" + '    this.HAXWiring.setup(' + this.props.elementClassName + '.haxProperties, ' + this.props.elementClassName + '.tag, this);';
       // set baseline for HAX schema
       this.props.haxList = {
         'canScale': true,
