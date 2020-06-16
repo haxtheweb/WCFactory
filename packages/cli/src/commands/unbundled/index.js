df05d555fc9a52ff13f02e0c32a41408e3def651
heymp <mgp140@gmail.com>
2020-06-16 09:59:57 -0400

    trimmed up

--- packages/cli/src/commands/unbundled/index.js
+++ packages/cli/src/commands/unbundled/index.js
@@ -1,20 +1,13 @@
 const { Command, flags } = require('@oclif/command')
-// const { promptUser } = require('../utils/commands')
-// var yeoman = require('yeoman-environment')
-// var env = yeoman.createEnv()
-// env.register(require.resolve('@wcfactory/generator-wcfactory/generators/unbundled-build'), 'wcfactory:unbundled-build')
 
 class Unbundled extends Command {
   async run() {
     let { args, flags } = this.parse(Unbundled)
-    // prompt the user for remaining flags
-    // flags = await promptUser(questions, flags, this)
-    // kick off the generator
-    // env.run('wcfactory:unbundled-build', flags)
+    console.log(`You must specify either unbundled:generate or unbundled:build`)
   }
 }
 
-Unbundled.description = 'Generate Unbundled Builds. A method of packaging web components for production.'
+Unbundled.description = '[In Development] Generate Unbundled Builds. A method of packaging web components for production.'
 
 /**
  * @todo dynaimically generate this based on the questions const
